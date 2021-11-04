ed65d0856cc7c66eeccfdd91cef9a444242bff93
AuroraMaestro <ahmedhz2013@gmail.com>
2021-11-04 20:22:20 +0200

    merge with corg

--- backend/Controllers/AdminController.js
+++ backend/Controllers/AdminController.js
@@ -33,9 +33,103 @@ exports.searchFlight = async function(req,res) {
 
 exports.getAllFlights = async function(req,res) {
 
-    const flightResults = await Flight.find({}).exec();
-
-    res.status(200).send(flightResults);
+    await Flight.find({})
+            .then( (flights) => {
+                res.status(200)
+                res.json(flights)
+            })
+            .catch( (err) => {
+                res.status(404)
+                console.log(err)})
 
     // then send it to FE.
 }
+
+exports.getFlightById = async function(req,res) {
+
+    let ID = req.params.getID
+
+    await Flight.findById(ID)
+    .then( (flights) => {
+        res.status(200)
+        res.json(flights)
+    })
+    .catch( (err) => {
+        res.status(404)
+        console.log(err)})
+}
+
+// router.get("/:getID", (req, res) =>
+
+
+exports.newFlight = async function(req,res) {
+
+    let newFlight = new Flight(req.body.flight);
+
+    await newFlight.save()
+        .then( (flight) => {
+            res.status(200)
+            res.json(flight)
+        })
+        .catch( (err) => {
+            if(err.name === "ValidationError") {
+                let errors = {};
+          
+                Object.keys(err.errors).forEach((key) => {
+                  errors[key] = err.errors[key].message;
+                });
+          
+                return res.status(400).send(errors);
+              }
+
+            res.status(500).send(err.name)
+            console.log(err.message)})
+}
+
+// Create
+// router.post("/", (req, res) => {
+
+
+exports.updateFlightById = async function(req,res) {
+
+    let ID = req.params.updateID;
+
+    await Flight.findByIdAndUpdate(ID, req.body.flight, {new: true})
+        .then( (flights) => {
+            res.status(200)
+            res.json(flights)
+        })
+        .catch( (err) => {
+            if (err.name === "ValidationError") {
+                let errors = {};
+          
+                Object.keys(err.errors).forEach((key) => {
+                  errors[key] = err.errors[key].message;
+                });
+          
+                return res.status(400).send(errors);
+              }
+
+            res.status(500).send(err.name)
+            console.log(err.message)})
+}
+
+// Update
+// router.put("/:updateID", (req, res) => {
+
+exports.deleteFlightById = async function(req,res) {
+
+    let ID = req.params.deleteID;
+
+    await Flight.findByIdAndDelete(ID)
+        .then( (flights) => {
+            res.status(200)
+            res.json(flights)
+        })
+        .catch( (err) => {
+            res.status(404)
+            console.log(err)})
+}
+
+// Delete
+// router.delete("/:deleteID", (req, res) => {
