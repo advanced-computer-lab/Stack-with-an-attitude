1efbe7bdd71b52154439f23317aca003994585cc
AuroraMaestro <ahmedhz2013@gmail.com>
2021-11-07 21:15:11 +0200

    merge

--- frontend/my-app/src/Updateflight.js
+++ frontend/my-app/src/Updateflight.js
@@ -1,95 +1,65 @@
-import React, { useEffect, useState } from 'react'
-import {Link,useParams} from 'react-router-dom'
-import axios from 'axios';
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
-import SendIcon from '@mui/icons-material/Send';
+import React, {Component} from 'react'
+import {Link} from 'react-router-dom'
+import axios from 'axios' ;
 
-
-
-
-function Updateflight(){
-  const [updated,setUpdated]=useState(false);
-  const [flight,setFlight] = useState([]);
-  const {id} = useParams();
-
-
-  const handleSubmit=(e)=>{
-    e.preventDefault();
-    const update = {
-      "flightNumber":e.target.flightNumber.value,
-      "departureTime":e.target.departureTime.value,
-      "arrivalTime":e.target.arrivalTime.value,
-      "date":e.target.date.value,
-      "economySeats":e.target.economySeats.value,
-      "businessSeats":e.target.businessSeats.value,
-      "from":e.target.from.value,
-      "to":e.target.to.value
-    }
-    console.log(update);
-    axios.put(`http://localhost:8000/updateFlight/${id}`,{flight:update})
-    .then(data=>{
-      console.log(data.data);
-      console.log("updated successfully")
-
-      e.target.flightNumber.value='';
-  e.target.departureTime.value='';
-  e.target.arrivalTime.value='';
-  e.target.date.value='';
-  e.target.economySeats.value='';
-  e.target.businessSeats.value='';
-  e.target.from.value='';
-  e.target.to.value='';
-    setUpdated(true);
-    }).catch(error=>{
-      console.log(error)
-    })
-  }
-
-  useEffect(()=>{
-  },[updated])
-
-  useEffect(()=>{
-    async function fetchData(){
-    let data = (await axios.get(`http://localhost:8000/getFlight/${id}`)).data
-    setFlight(data);
-    console.log(data);
-    }
-
-
-    fetchData();
-  },[id])
+class Addflights extends Component{
   
-  useEffect(()=>{
-  },[flight])
+  submit(e){
+    e.preventDefault();
+    const flight = {
+      "flightNumber": e.target.fnum.value,
+      "departureTime": e.target.deptime.value,
+      "arrivalTime": e.target.arrtime.value,
+      "date": e.target.date.value,
+      "economySeats": e.target.ecseats.value,
+      "businessSeats": e.target.busseats.value,
+      "from": e.target.fromf.value,
+      "to": e.target.to.value,
+    }
+     
+    
+axios.post('http://localhost:8000/createFlight',{"flight" : flight}).then((data) => {
+  console.log("success");
+console.log(data)
+}).catch(err => console.log(err));
 
+
+  }  
+  render(){
       return(
         <div>
 
+
           <Link to='/'><h2>Home</h2></Link>
           <br/>
-        <h1>update flight with flight number {flight.flightNumber}</h1> 
-        {updated && <h2 className="feedback-header">updated flight successfully </h2>}
-        <form onSubmit={handleSubmit} id="form">
-          {(Object.keys(flight).slice(1,9)).map((f)=>(
-          <TextField
-          required
-          key={f}
-          id={f}
-          label={f}
-          name={f}
-          defaultValue={flight[f]}
-          />
-          ))}
-         
-          <Button value="Submit" type="submit" variant="contained" endIcon={<SendIcon />}>
-              Submit
-          </Button>
+        <form onSubmit={this.submit} id="form">
+          <label >flight number:</label><br/>
+          <input type="text" id="fnum" name="fnum" /><br/>
+          <label >departure time:</label><br/>
+          <input type="text" id="deptime" name="deptime"/><br/>
+          <label >arrival time:</label><br/>
+          <input type="text" id="arrtime" name="arrtime"/><br/>
+          <label>date:</label><br/>
+          <input type="text" id="date" name="date"/><br/>
+          <label >number of economy seats:</label><br/>
+          <input type="text" id="ecseats" name="ecseats"/><br/>
+          <label >number of business seats:</label><br/>
+          <input type="text" id="busseats" name="busseats"/><br/>
+          <label >airport:</label><br/>
+          <input type="text" id="airport" name="airport"/><br/>
+          <label >from:</label><br/>
+          <input type="text" id="fromf" name="fromf"/><br/>
+          <label >to:</label><br/>
+          <input type="text" id="to" name="to"/><br/>
+          <input type="submit" value="Submit" />
         </form>
         </div>
-
+     
       );
     }
   
 
-export default Updateflight ;
+
+}
+
+export default Addflights ;
