c37d9bf6509dc7e7ebcc1da7a2975f98ad3347de
AuroraMaestro <ahmedhz2013@gmail.com>
2021-12-01 23:00:41 +0200

    added routes in app.js

--- backend/app.js
+++ backend/app.js
@@ -62,6 +62,10 @@ app.put('/updateFlight/:updateID', adminController.updateFlightById);
 app.post('/createFlight',adminController.newFlight);
 
 app.delete('/deleteFlight/:deleteID',adminController.deleteFlightById);
+
+app.get('/allreservedflights', adminController.getAllreservedFlights);
+
+app.delete('/deletereservedFlight/:deleteID',adminController.deletereservedflight);
 //-------------
 
 //------------User
@@ -70,17 +74,15 @@ app.put('/user/reserveSeats/:id',userController.reserveSelectedSeats);
 app.put('/user/update/:id', userController.updateUserById);
 
 app.get('/user/getInfo/:id', userController.getUserById);
+
+app.get('/user/reservedSeats', userController.getAllReservedSeats);
+
+app.get('/user/reservedFlight/:id', userController.getReservedFlightById);
+
+app.post('/user/createReservedFlight', userController.createReservedFlight);
+
+app.delete('/user/deleteReservedFlight/:id', userController.deleteReservedFlightById);
 //--------------
-
-
-
-
-app.get('/allreservedflights', adminController.getAllreservedFlights);
-
-app.delete('/deletereservedFlight/:deleteID',adminController.deletereservedflight);
-
-
-
 
 //for login we store ONLY and ONLY I SAY AGAIN the USERNAME or ID not the password , NEVER!!!
 
