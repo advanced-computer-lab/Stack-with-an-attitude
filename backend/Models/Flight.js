a1660ed86d5929fe1d7bcb8ce898855d323d9ddb
GeorgeKamal <GEORGEKAMAL@mail.com>
2021-12-03 18:50:29 +0200

    FIXED THE EMPTY ARRAY FINALLY

--- backend/Models/Flight.js
+++ backend/Models/Flight.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+
 const flightSchema = new Schema ({
     flightNumber: {
         type: String,
@@ -53,7 +54,12 @@ const flightSchema = new Schema ({
     totalSeats: {
         type: Number,
         required: true,
-        default: ()=> {economySeats + businessSeats},
+        default: function() {
+            if (this.economySeats && this.businessSeats) {
+              return this.economySeats + this.businessSeats;
+            }
+            return null;
+          },
         validate: {
             message: "Must be a Positive Number",
             validator: (input) => {
@@ -107,27 +113,39 @@ const flightSchema = new Schema ({
     reservedBusinessSeats: {
         type: Array,
         required: true,
-        default: ()=> {
-                        let seatsArray = [];
-                        for (let i = 0; i < businessSeats; i++) {
-                            seatsArray[i] = false; 
-                        }
-                        },
+        default: function() {
+            if (this.businessSeats) {
+                var seats = [];
+                for(let i=0; i<this.businessSeats; i++){
+                    seats[i] = false;
+                }
+              return seats;
+            }
+            return [];
+          }
     },
     reservedEconomySeats: {
         type: Array,
         required: true,
-        default: ()=> {
-                        let seatsArray = [];
-                        for (let i = 0; i < economySeats; i++) {
-                            seatsArray[i] = false; 
-                        }
-                        },
+        default: function() {
+            if (this.economySeats) {
+                var seats = [];
+                for(let i=0; i<this.economySeats; i++){
+                    seats[i] = false;
+                }
+              return seats;
+            }
+            return [];
+          }
     },
     availableBusinessSeats: {
         type : Number,
         required: true,
-        default: ()=> {businessSeats - reservedBusinessSeats},
+        default: function() {
+            if (this.businessSeats && this.reservedBusinessSeats) {
+                return this.businessSeats - this.reservedBusinessSeats.filter(x => x).length 
+            }
+          },
         validate: {
             message: "Must be a Positive Number",
             validator: (input) => {
@@ -138,7 +156,11 @@ const flightSchema = new Schema ({
     availableeconomySeats: {
         type : Number,
         required: true,
-        default: ()=> {economySeats - reservedEconomySeats },
+        default: function() {
+            if (this.economySeats && this.reservedEconomySeats) {
+                return this.economySeats - this.reservedEconomySeats.filter(x => x).length 
+            }
+          },
         validate: {
             message: "Must be a Positive Number",
             validator: (input) => {
@@ -147,7 +169,6 @@ const flightSchema = new Schema ({
         }
     }
 }, {timestamps:true})
-
 
 // function that validate the startDate and endDate
 function dateValidator(value) {
